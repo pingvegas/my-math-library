344388ff6991299d2468c1f08c81b87854e4174e
Phannatorn Seswisaratakul <wweping@gmail.com>
2021-02-09 11:55:20 +0700

    Refactor to use array.forEach

--- my-math-library/math.js
+++ my-math-library/math.js
@@ -1,9 +1,9 @@
+
+
 function sum(a,b){
     if(Array.isArray(a)){
         let total = 0 
-        for(let i = 0 ; i < a.length;i++){
-            total+=a[i]
-        }
+        a.forEach((n)=>(total+=n))
         return total
     }
 
