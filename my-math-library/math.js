479b2ffa725e9416b0b6a22d6799d7539fc6d057
Phannatorn Seswisaratakul <wweping@gmail.com>
2021-02-09 11:48:32 +0700

    Add sum array functionality

--- my-math-library/math.js
+++ my-math-library/math.js
@@ -1,6 +1,15 @@
 function sum(a,b){
+    if(Array.isArray(a)){
+        let total = 0 
+        for(let i = 0 ; i < a.length;i++){
+            total+=a[i]
+        }
+        return total
+    }
+
     return a+b
 }
+
 module.exports = {
     sum,
 }
--- my-math-library/math.test.js
+++ my-math-library/math.test.js
@@ -1,4 +1,5 @@
 const math = require('./math')
 const assert = require('assert')
 
-assert.strictEqual(math.sum(2,2),4)
+assert.strictEqual(math.sum(2, 2),4)
+assert.strictEqual(math.sum([1,2,3,4,5]),15)
