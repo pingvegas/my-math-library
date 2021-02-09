675036c8a859a9bf616915203052927c1caf8dd2
Phannatorn Seswisaratakul <wweping@gmail.com>
2021-02-09 12:00:17 +0700

    Refactor to use array.redude

--- my-math-library/math.js
+++ my-math-library/math.js
@@ -2,9 +2,7 @@
 
 function sum(a,b){
     if(Array.isArray(a)){
-        let total = 0 
-        a.forEach((n)=>(total+=n))
-        return total
+       return a.reduce((total,n)=>total + n , 0)
     }
 
     return a+b
